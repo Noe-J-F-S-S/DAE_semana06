a75388ba3ef4ee2111be47d77f396d00971993f9
Noe <noe@gmail.com>
2022-04-20 09:14:32 -0500

    first commit

---
+++ tienda/views.py
@@ -0,0 +1,21 @@
+from multiprocessing import context
+from django.shortcuts import get_object_or_404,render
+
+from .models import Producto, Categoria
+# Create your views here.
+
+def index(request):
+    product_list = Producto.objects.order_by('nombre')[:6]
+    category_list=Categoria.objects.order_by('nombre')
+    context = {'product_list': product_list,'category_list':category_list}
+    return render(request,'index.html',context)
+
+def producto(request, producto_id):
+    producto= get_object_or_404(Producto, pk=producto_id)
+    category_list=Categoria.objects.order_by('nombre')
+    return render(request, 'producto.html', {'producto': producto,'category_list':category_list})
+
+def categoria(request,categoria_id):
+    productos = Producto.objects.filter(catergoria=categoria_id)
+    category_list = Categoria.objects.order_by('nombre')
+    return render(request, 'categoria.html', {'productos':productos,'category_list':category_list})
